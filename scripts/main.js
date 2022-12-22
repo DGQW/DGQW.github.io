afc2a702cde4fab37d2bb56889062201ea6f7113
David <20230139@tsinglan.cn>
2022-12-22 22:06:50 +0800

    all files are added# Please enter the commit message for your changes. Lines starting

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/we-service.jpeg') {
+      myImage.setAttribute('src', 'images/we-service2.jpeg');
+    } else {
+      myImage.setAttribute('src', 'images/we-service.jpeg');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myParagraph = document.querySelector('p');
+
+function setUserName() {
+    let myInfo = prompt('请输入你想显示的信息。');
+    localStorage.setItem('information', myInfo);
+    myParagraph.textContent = myInfo;
+  }
+
+myButton.onclick = function() {
+    setUserName();
+ }
